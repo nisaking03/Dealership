a0ab9e1a147238680725995d121673f50470e31f
nisaking03 <nisa.king03@gmail.com>
2025-10-24 05:46:31 -0400

    Making classes

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
+/* Features this will Include
+
+1 - Find vehicles within a price range
+2 - Find vehicles by make / model
+3 - Find vehicles by year range
+4 - Find vehicles by color
+5 - Find vehicles by mileage range
+6 - Find vehicles by type (car, truck, SUV, van)
+7 - List ALL vehicles
+8 - Add a vehicle
+9 - Remove a vehicle
+99 - Quit */
